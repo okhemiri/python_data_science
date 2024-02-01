dba70644d1b56d029e336735c6174709ad4c56d4
okhemiri <oussema13khemiri@gmail.com>
2024-02-01 15:34:13 +0100

    exemple1 : variables, types de base, print

---
+++ seance_1/exemple1.py
@@ -0,0 +1,10 @@
+# En mode script, on écrit les codes dans un fichier .py
+
+nom="Ali" # variable de type str
+age=30 # variable de type int
+salaire=1200.7654 # variable de type float
+est_celibataire=True # variable de type booléen
+# Afficher les informations de la personne
+# On appelle print
+print('L\'employé "',nom,"\" a comme salaire ",salaire,
+      " et comme age ",age," celibataire: ",est_celibataire, sep="")
